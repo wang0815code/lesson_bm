68a61fcf74f5e61f079a4d9a12adad2069edb35a
wang0815code <62864723+wang0815code@users.noreply.github.com>
2020-04-14 21:30:20 +0800

    爬楼梯算法

---
+++ 4.js
@@ -0,0 +1,15 @@
+function f(n){
+    if (n==1) return 1;
+    if (n==2) return 2;
+    let ret=0,
+      pre=2,
+      prepre=1;
+    for(let i=3;i<=n;i++){
+        ret=pre+prepre;
+        prepre=pre;
+        pre=ret;
+    }  
+    return ret;
+}
+
+console.log(f(100));
